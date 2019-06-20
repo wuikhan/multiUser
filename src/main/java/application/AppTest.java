1393aa3e668b66503f533593300ab648665b30d4
waqas.khan <waqas.khan@accenturefederal.com>
2019-06-20 19:11:45 -0400

    ~my~app

--- src/main/java/application/AppTest.java
+++ src/main/java/application/AppTest.java
@@ -34,6 +34,8 @@ public class AppTest {
 	@AfterMethod
 	public void tearDown() {
 		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
 		driver.quit();
 	}
 }
